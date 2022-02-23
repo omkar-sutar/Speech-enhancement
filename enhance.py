7569cda86cb1853769f6dc7a57bb64daaaa74ee9
omkar-sutar <omkartsutar2001@gmail.com>
2022-02-23 13:26:34 +0530

    modified for gui support

--- enhance.py
+++ enhance.py
@@ -1,18 +1,24 @@
+import subprocess
 import os
-input_directory='./mypredictions/input'
-output_directory='./mypredictions/output'
+from queue import Queue
+from sys import stdout
+from prediction_denoise import prediction
+from variables import *
 
-
-input_files=os.listdir(input_directory)
-
-def predict_all()->None:
+def predict_all(input_files:list,input_dir:str,messageQueue:Queue)->None:
+    messageQueue.put("Process started")
     for input_file in input_files:
-        output_file="out"+input_file
-        print(os.popen(f'python main.py --audio_dir_prediction .\mypredictions\input\ --dir_save_prediction .\mypredictions\output\ --audio_input_prediction {input_file} --audio_output_prediction {output_file}').read())
+        messageQueue.put("Processing "+input_file)
+        output_file=os.path.join(os.path.dirname(input_file),"output_"+os.path.basename(input_file))
+        print(output_file)
+        #process=subprocess.Popen(f'python main.py --audio_dir_prediction .\mypredictions\input\ --dir_save_prediction .\mypredictions\output\ --audio_input_prediction {input_file} --audio_output_prediction {output_file}',stdout=subprocess.PIPE)
+        prediction(weights_path=weights_path,name_model=name_model,
+                    audio_input_prediction=input_file,audio_output_prediction=output_file,
+                    audio_dir_prediction=input_dir,dir_save_prediction=input_dir,
+                    sample_rate=sample_rate,min_duration=min_duration,frame_length=frame_length,
+                    hop_length_frame=hop_length_frame,n_fft=n_fft,hop_length_fft=hop_length_fft,messageQueue=messageQueue)
+    messageQueue.put("Process finished")
 
 def predict(input_filename:str)->str:
     output_file="out"+input_filename
     return os.popen(f'python main.py --audio_dir_prediction .\mypredictions\input\ --dir_save_prediction .\mypredictions\output\ --audio_input_prediction {input_filename} --audio_output_prediction {output_file}').read()
-
-if __name__ == '__main__':
-    predict_all()
