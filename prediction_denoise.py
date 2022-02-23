2543b1f3a573f25db8c975226a5456f7a3b1ae36
omkar-sutar <omkartsutar2001@gmail.com>
2022-02-23 13:26:58 +0530

    modified for gui support

--- prediction_denoise.py
+++ prediction_denoise.py
@@ -4,9 +4,11 @@ from tensorflow.keras.models import model_from_json
 from data_tools import scaled_in, inv_scaled_ou
 from data_tools import audio_files_to_numpy, numpy_audio_to_matrix_spectrogram, matrix_spectrogram_to_numpy_audio
 import soundfile as sf
+import os
+from queue import Queue
 
 def prediction(weights_path, name_model, audio_dir_prediction, dir_save_prediction, audio_input_prediction,
-audio_output_prediction, sample_rate, min_duration, frame_length, hop_length_frame, n_fft, hop_length_fft):
+audio_output_prediction, sample_rate, min_duration, frame_length, hop_length_frame, n_fft, hop_length_fft,messageQueue:Queue):
     """ This function takes as input pretrained weights, noisy voice sound to denoise, predict
     the denoise sound and save it to disk.
     """
@@ -18,7 +20,7 @@ audio_output_prediction, sample_rate, min_duration, frame_length, hop_length_fra
     loaded_model = model_from_json(loaded_model_json)
     # load weights into new model
     loaded_model.load_weights(weights_path+'/'+name_model+'.h5')
-    print("Loaded model from disk")
+    messageQueue.put("Loaded model from disk")
 
     # Extracting noise and voice from folder and convert to numpy
     audio = audio_files_to_numpy(audio_dir_prediction, audio_input_prediction, sample_rate,
@@ -26,7 +28,7 @@ audio_output_prediction, sample_rate, min_duration, frame_length, hop_length_fra
 
     #Dimensions of squared spectrogram
     dim_square_spec = int(n_fft / 2) + 1
-    print(dim_square_spec)
+    messageQueue.put("Dimensions of squared spectrogram: "+str(dim_square_spec))
 
     # Create Amplitude and phase of the sounds
     m_amp_db_audio,  m_pha_audio = numpy_audio_to_matrix_spectrogram(
@@ -37,20 +39,27 @@ audio_output_prediction, sample_rate, min_duration, frame_length, hop_length_fra
     #Reshape for prediction
     X_in = X_in.reshape(X_in.shape[0],X_in.shape[1],X_in.shape[2],1)
     #Prediction using loaded network
+    messageQueue.put("Predicting noise")
     X_pred = loaded_model.predict(X_in)
     #Rescale back the noise model
     inv_sca_X_pred = inv_scaled_ou(X_pred)
     #Remove noise model from noisy speech
+    messageQueue.put("Removing noise")
     X_denoise = m_amp_db_audio - inv_sca_X_pred[:,:,:,0]
     #Reconstruct audio from denoised spectrogram and phase
+    messageQueue.put("Reconstructing audio...")
     print(X_denoise.shape)
     print(m_pha_audio.shape)
     print(frame_length)
     print(hop_length_fft)
     audio_denoise_recons = matrix_spectrogram_to_numpy_audio(X_denoise, m_pha_audio, frame_length, hop_length_fft)
+    messageQueue.put("Denoised audio shape: "+str(audio_denoise_recons.shape))
     #Number of frames
     nb_samples = audio_denoise_recons.shape[0]
     #Save all frames in one file
     denoise_long = audio_denoise_recons.reshape(1, nb_samples * frame_length)*10
     #librosa.output.write_wav(dir_save_prediction + audio_output_prediction, denoise_long[0, :], sample_rate)
-    sf.write(dir_save_prediction + audio_output_prediction,denoise_long[0, :], sample_rate,'PCM_24')
+    messageQueue.put("Writing audio to output file...")
+    print(dir_save_prediction)
+    print(audio_output_prediction)
+    sf.write(os.path.join(dir_save_prediction,audio_output_prediction),denoise_long[0, :], sample_rate,'PCM_24')
